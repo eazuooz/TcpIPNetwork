651a013ae3c6e5aae64a7c9f3c7cfcca2b550fda
aquarius4785@gmail.com <aquarius4785@gmail.com>
2022-08-15 16:53:40 +0900

    thread

---
+++ Thread/Thread.cpp
@@ -0,0 +1,40 @@
+﻿// Thread.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <process.h>
+
+unsigned WINAPI ThreadFunc(void* arg)
+{
+	int i;
+	int cnt = *((int*)arg);
+	for (i = 0; i < cnt; i++)
+	{
+		Sleep(10000);  puts("running thread");
+	}
+	return 0;
+}
+
+int main()
+{
+    HANDLE hThread;
+    unsigned int threadID;
+    int param = 5;
+
+	hThread = (HANDLE)_beginthreadex
+				(NULL, 0, ThreadFunc, (void*)&param, 0, &threadID);
+
+	if (hThread == 0)
+	{
+		puts("_beginthreadex() error");
+		return -1;
+	}
+
+	Sleep(30000);
+	puts("end of main");
+
+    return 0;
+}
+
+
---
+++ Thread2/Thread2.cpp
@@ -0,0 +1,46 @@
+﻿// Thread2.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <Windows.h>
+#include <process.h>
+
+unsigned WINAPI ThreadFunc(void* arg)
+{
+	int i;
+	int cnt = *((int*)arg);
+	for (i = 0; i < cnt; i++)
+	{
+		Sleep(1000);  puts("running thread");
+	}
+	return 0;
+}
+
+int main()
+{
+	HANDLE hThread;
+	unsigned int threadID;
+	int param = 5;
+
+	hThread = (HANDLE)_beginthreadex
+	(NULL, 0, ThreadFunc, (void*)&param, 0, &threadID);
+
+	if (hThread == 0)
+	{
+		puts("_beginthreadex() error");
+		return -1;
+	}
+
+	DWORD wr = WaitForSingleObject(hThread, INFINITE);
+
+	if (wr == WAIT_FAILED)
+	{
+		puts("thread wait error");
+		return -1;
+	}
+
+	printf("wait result: %s \n", (wr == WAIT_OBJECT_0) ? "signaled" : "time-out");
+	puts("end of main");
+
+	return 0;
+}
